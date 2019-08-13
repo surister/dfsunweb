141883f09561bab9e3af9e3484b14e6b816b224b
surister <surister98@gmail.com>
2019-08-13 17:33:44 +0200

    update

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
-# driver = webdriver.Firefox(executable_path='D:\geckodriver\geckodriver.exe')
-driver = webdriver.phantomjs
+from time import sleep
+
+driver = webdriver.Firefox(executable_path='D:\geckodriver\geckodriver.exe')
+
 
 driver.get("http://checkyourflight.info")
-departdate = driver.find_element_by_id('fecha_salida')
-departdate.send_keys('10/08/2019')
-print(departdate)
-select = Select(driver.find_element_by_id('flight'))
+driver.find_element_by_id('fecha_salida').click()
+driver.find_element_by_id('fecha_salida').send_keys('2019-08-14')
+driver.find_element_by_id('fecha_salida').click()
+sleep(1)
+flight_element = driver.find_element_by_xpath("//select[@id='flight']")
+flights = flight_element.find_elements_by_tag_name("option")
+for flight in flights:
+    flight.click()
+    print(flight.get_attribute('value'))
+    sleep(1)
+    element_hotel = driver.find_element_by_xpath("//select[@id='hoteles']")
+    hotels = element_hotel.find_elements_by_tag_name("option")
+    for hotel in hotels:
+        hotel.click()
+        print(hotel.get_attribute('value'))
+sleep(100)
 
-for option in select:
-    print("Value is: %s" % option.get_attribute("value"))
-    option.click()
 driver.close()
